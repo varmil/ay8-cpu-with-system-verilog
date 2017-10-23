87e1c65bd8042e716b3a546483045c6092ebaff1
山本　晃大 <cf0293@CYGAMES-1818.local>
2017-10-23 21:10:03 +0900

    [WIP] fix nop machine behaviour

--- verilog/ay8.sv
+++ verilog/ay8.sv
@@ -1,38 +1,8 @@
 module NopMachine(IMemory intf);
   parameter HIGH_IMPEDANCE = 1'bZ;
 
-  // typedef enum logic [1:0] { ST0, ST1 } stateEnum;
-  // stateEnum state = ST0;
-
   logic [7:0] pc = 8'b0000_0000;
   logic [7:0] dummyReg = 8'b0000_0000;
-
-  // always_ff @ (posedge intf.CLK, negedge intf.RST) begin
-  //   if (intf.RST == 0) begin
-  //     state <= ST0;
-  //     pc <= 0;
-  //   end
-  //   else begin
-  //     case (state)
-  //       ST0 : begin
-  //               intf.exec(intf.READ, pc);
-  //               state <= ST1;
-  //             end
-  //       ST1 : begin
-  //               pc <= pc + 1;
-  //               state <= ST0;
-  //             end
-  //     endcase
-  //   end
-  // end
-  //
-  // always_ff @ (posedge intf.CLK, negedge intf.RST) begin
-  //   // このとりこみがuniBusの値反映を待たずに行われているのが問題
-  //   dummyReg <= intf.uniBus;
-  // end
-
-
-
 
   enum {
     ST0 = 0,
--- verilog/memory.sv
+++ verilog/memory.sv
@@ -29,10 +29,8 @@ module Memory(IMemory intf);
 
   logic [7:0] mem[255:0];
   logic [1:0] state, readState, writeState;
-  logic shouldOutput;
 
   function void clearState();
-    shouldOutput = 1'b0;
     intf.isRunning <= intf.NOT_RUNNING;
     intf.rW <= HIGH_IMPEDANCE;
   endfunction
@@ -45,7 +43,6 @@ module Memory(IMemory intf);
       // ノンブロッキング代入する前に条件分岐を終えてる感じ
       case (state)
         readState : begin
-                      shouldOutput <= 1'b1;
                       clearState();
                     end
         writeState : begin
@@ -54,14 +51,76 @@ module Memory(IMemory intf);
                     end
         default : ;
       endcase
-
-      // output for uniBus should continue 1 cycle
-      if (shouldOutput) shouldOutput <= ~shouldOutput;
     end
   end
+
+
+
+
+
+  // enum {
+  //   IDLE = 0,
+  //   READ = 1,
+  //   WRITE = 2
+  // } state_index_t;
+  // logic [3:0] state, next;
+  //
+  // // Sequential state transition
+  // always_ff @(posedge intf.CLK, negedge intf.RST) begin
+  //   if (!intf.RST) begin
+  //     state       <= '0; // default assignment
+  //     state[IDLE] <= 1'b1;
+  //   end
+  //   else
+  //     state       <= next;
+  // end
+  //
+  // // Combinational next state logic
+  // always_comb begin
+  //   next = '0;
+  //   unique case (1'b1)
+  //     state[IDLE]  : begin
+  //       if (condition1) begin
+  //
+  //       end else if (${2:condition2) begin
+  //
+  //       end else begin
+  //
+  //       end
+  //       if (intf.rW ==) next[READ] = 1'b1;
+  //       if (goWrite) next[WRITE] = 1'b1;
+  //     end
+  //     state[READ]  : next[IDLE] = 1'b1;
+  //     state[WRITE] : next[IDLE] = 1'b1;
+  //     default      : next[IDLE] = 1'b1;
+  //   endcase
+  // end
+  //
+  // // Make output assignments
+  // always_ff @(posedge intf.CLK, negedge intf.RST) begin
+  //   if (!intf.RST) begin
+  //   end
+  //   else begin
+  //     unique case (1'b1)
+  //       state[IDLE]  : begin
+  //       end
+  //       state[READ]  : begin
+  //       end
+  //       state[WRITE] : begin
+  //       end
+  //       default: ;
+  //     endcase
+  //   end
+  // end
+
+
+
+
+
+
 
   assign state = { intf.isRunning, intf.rW };
   assign readState = { intf.NOW_RUNNING, intf.READ };
   assign writeState = { intf.NOW_RUNNING, intf.WRITE };
-  assign intf.uniBus = (shouldOutput) ? mem[intf.addressBuff] : { 8{HIGH_IMPEDANCE} };
+  assign intf.uniBus = (state == readState) ? mem[intf.addressBuff] : { 8{HIGH_IMPEDANCE} };
 endmodule
